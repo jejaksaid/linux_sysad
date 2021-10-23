60d0be24de570a934c9677878ba091c1c2ce42f5
jejaksaid <said@pop-os.localdomain>
2021-10-24 06:59:24 +0700

    added ghcommand.sh to master

---
+++ ghcommand.sh
@@ -0,0 +1,14 @@
+1. Git Init
+the first command your need to run in any new project that is not already a Git repository.
+it can be used to convert existing, un-versioned folder into a Git repo.
+Also, you can use it to initialize an empty repo.
+
+Using Git Init Command
+
+cd into the dir you want to initialize.
+
+then run this command.
+
+git init
+
+this will transform the current dir into a Git repo. A .git sub-dir will be added.
